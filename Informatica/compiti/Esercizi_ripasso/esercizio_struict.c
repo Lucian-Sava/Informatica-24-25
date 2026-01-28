8b59dec881d137d2b8d2221a6ac6fd1c4d256be4
Lucian-Sava <georgelucian.sava@galileiostiglia.edu.it>
2026-01-28 19:38:06 +0000

    Stringhe

--- Informatica/compiti/Esercizi_ripasso/esercizio_struict.c
+++ Informatica/compiti/Esercizi_ripasso/esercizio_struict.c
@@ -10,6 +10,9 @@ typedef struct {
 int main() {
     Auto concessionaria[100]; // Array di struct, la struct Auto la definisco concettualmente come una "nuova variabile", che e la concessionaria
     int n, i;
+    int Maprezzo;
+    int Posizione;
+    int trovato = 0;
 
     // 1. Quante auto inserire?
     printf("Quante auto vuoi inserire? ");
@@ -26,17 +29,58 @@ int main() {
         printf("Prezzo: ");
         scanf("%f", &concessionaria[i].prezzo); // Qui serve la &
     }
+
+        //printf("Il cliente Dice il massimo di prezzo che vuoi spendere: ");
+        //scanf("%d", &Maprezzo);
     // 3. Esempio di elaborazione: Stampa auto costose (> 10000)
     printf("\n--- AUTO COSTOSE ---\n");
     for(i = 0; i < n; i++) 
-   {
-        // Accesso ai campi con il punto
-        if(concessionaria[i].prezzo > 10000) 
-        {
-            printf("Auto: %s %s - Euro: %.2f\n", 
-            concessionaria[i].marca, 
-            concessionaria[i].modello, 
-            concessionaria[i].prezzo);
-        }
+   {    
+        /*
+            METODO 1 : uso il confronto diretto
+
+                // Accesso ai campi con il punto
+                if(concessionaria[i].prezzo > 10000) 
+                {
+                    printf("Auto: %s %s - Euro: %.2f\n", 
+                    concessionaria[i].marca, 
+                    concessionaria[i].modello, 
+                    concessionaria[i].prezzo);
+                }
+        */
+
+        /*
+            METODO 2 : uso il confronto di stringhe
+                if(strcmp(concessionaria[i].marca, "Citroen") == 0)
+                {
+                    printf("Ho trovato una Citroen: %s\n", concessionaria[i].marca);
+                    printf("Il modello è: %s\n", concessionaria[i].modello);
+                }
+        */
+        /*
+            METODO 3 : uso il confronto di stringhe con minore o amggiore del prezzo
+                if(concessionaria[i].prezzo <= Maprezzo) 
+                {
+                    Maprezzo = concessionaria[i].prezzo;
+                    Posizione = i;
+                    printf("L'auto affidabile è: %s %s - Euro: €%.2f\n", 
+                    concessionaria[Posizione].marca, 
+                    concessionaria[Posizione].modello, 
+                    concessionaria[Posizione].prezzo);
+                }
+        */
+
+        /* 
+            METODO 4 : uso flag (trovato)
+                    if(strcmp(concessionaria[i].marca, "Fiat") == 0)
+                    {
+                        printf("Ho trovato una fiat: %s\n", concessionaria[i].modello);
+                        trovato = 1;
+                    }
+                    else if(trovato == 0)
+                    {
+                        printf("Non ho trovato una fiat in magazzino\n");
+                    }
+        */
    }return 0;
 }
