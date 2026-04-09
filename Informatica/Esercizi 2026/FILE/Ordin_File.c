24dca4b22ca803417540d22f2a2717f16bdda459
Lucian-Sava <georgelucian.sava@galileiostiglia.edu.it>
2026-04-09 06:07:23 +0000

    Aggiornamento e aggiunti

---
+++ Informatica/Esercizi 2026/FILE/Ordin_File.c
@@ -0,0 +1,56 @@
+/* scrivere una funzione che preso in ingresso il
+   nome del filecontenente interi, lo modifichi in
+   modo tale da contenere gli stessi numeri ma 
+   ordinati in senso decrescente
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+void OrdinaFile(char nomeFile[]){
+	FILE *Fp;
+	int vett[100], i, j, temp, n=0;
+	
+	Fp = fopen( nomeFile, "r");
+	if (Fp == NULL){
+		printf("errore apertura file\n");
+		return 1;
+	}
+	/* Lettura dei numeri dal file */
+	while ( fscanf(Fp, "%d", &vett[n]) == 1){
+		printf("elemento %d: %d \n", n+1, vett[n]);
+		n++;		
+	}
+	fclose(Fp);
+	//ordinamento del bubble sort
+	for(i=0; i<n-1; i++){
+		for(j=i+1; j<n; j++ ){
+			if(vett[i]< vett[j]){
+				temp =vett[i];
+				vett[i]=vett[j];
+				vett[j]=temp;
+			}
+			
+		}
+	}
+	/*riaprimo il file per scriverci i numeri ordinati */
+	Fp  = fopen( nomeFile, "w");
+	if (Fp == NULL){
+		printf("errore apertura file\n");
+		return 1;
+	}
+	for(i=0; i < n; i++){
+		fprintf(Fp, "%d ", vett[i]);
+	}
+	fclose(Fp);
+}
+
+
+
+int main(){
+	char nome[20];
+	
+	printf("Inserire il nome del file: ");
+	scanf("%s", nome);
+	
+	OrdinaFile(nome);
+}
